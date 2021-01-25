f9ee67f26ed83f6b550f78050807cd1fa24c2f2c
jazeeh <xia5co@gmail.com>
2021-01-26 03:21:57 +0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const c1 = document.querySelector('.c1');
+const c2 = document.querySelector('.c2');
+const c3 = document.querySelector('.c3');
+const c4 = document.querySelector('.c4');
+const c5 = document.querySelector('.c5');
+const c6 = document.querySelector('.c6');
+
+c1.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c1.style.background = myColor;
+});
+
+
+c2.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c2.style.background = myColor;
+});
+
+c3.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c3.style.background = myColor;
+});
+
+c4.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c4.style.background = myColor;
+});
+
+c5.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c5.style.background = myColor;
+});
+
+c6.addEventListener('click', function(){
+	let myColor = prompt('Pick your favorite color');
+	c6.style.background = myColor;
+});
+
